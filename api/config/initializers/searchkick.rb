83bcffb4de2bfe671013597431ac55926c71a480
ugifractal <ugidmtest@gmail.com>
2025-12-10 07:12:53 +0700

    Configurable elasticsearch url

--- api/config/initializers/searchkick.rb
+++ api/config/initializers/searchkick.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-ENV["ELASTICSEARCH_URL"] = if Rails.env.production?
+ENV["ELASTICSEARCH_URL"] ||= if Rails.env.production?
   "http://campsite-elasticsearch.internal:9200"
 else
   "http://localhost:9200"
